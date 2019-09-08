01aa15e06b25b2475b3b3f2b6cb6e34a048de38a
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-08 23:18:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const driversByName = function(drivers){
 }
 
 const totalRevenue = function (drivers) {
-  return drivers.reduce(function (accumulator, driver) {
-    return accumulator + driver.revenue;
+  return drivers.reduce(function (total, driver) {
+    return total + driver.revenue;
   }, 0);
 }
 
