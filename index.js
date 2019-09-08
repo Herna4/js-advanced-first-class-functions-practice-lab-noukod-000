ebd254d0af3eccd0aa0c651f6a4239c38e45b2df
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-08 23:10:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const logDriverNames = function(drivers) {
+   drivers.forEach(function (driver){
+     console.log(driver.name)
+   });
+};
+
+const logDriversByHometown = function(drivers, location){
+  drivers.forEach(function (driver){
+    if (driver.hometown == location){
+    console.log(driver.name)
+    }
+  });
+};
+
+const driversByRevenue = function (drivers){
+  return drivers.slice().sort(function(a ,b){
+    return a.revenue - b.revenue;
+  });
+  
+};
+
+const driversByName = function(drivers){
+  
+}
